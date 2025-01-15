737858965288682d069520170a5ac6c64c565b99
huji <389491@niuitmo.ru>
2025-01-15 17:23:43 +0300

    chore: gradle init

---
+++ backend/app/build.gradle.kts
@@ -0,0 +1,35 @@
+plugins {
+    application
+}
+
+repositories {
+    // Use Maven Central for resolving dependencies.
+    mavenCentral()
+}
+
+dependencies {
+    // This dependency is used by the application.
+    implementation(libs.guava)
+}
+
+testing {
+    suites {
+        // Configure the built-in test suite
+        val test by getting(JvmTestSuite::class) {
+            // Use JUnit4 test framework
+            useJUnit("4.13.2")
+        }
+    }
+}
+
+// Apply a specific Java toolchain to ease working on different environments.
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(17)
+    }
+}
+
+application {
+    // Define the main class for the application.
+    mainClass = "org.example.App"
+}
